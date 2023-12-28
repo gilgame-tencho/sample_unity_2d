54edbbaee556632a983cad3e18b1edf627ca4929
gilgame-tencho <tokubetugosetuchitai.ttiw@gmail.com>
2023-12-28 15:16:07 +0900

    add Enemy

--- Script/Player.cs
+++ Script/Player.cs
@@ -11,13 +11,18 @@ public class Player : MonoBehaviour
     [SerializeField, Header("ジャンプ速度")]
     private float _jumpSpeed;
 
+    [SerializeField, Header("空中ジャンプ回数")]
+    private int _jumpMaxCount;
+
     private Vector2 _inputDirection;
     private Rigidbody2D _rigid;
+    private int _bJump;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
+        _bJump = 0;
     }
 
     // Update is called once per frame
@@ -30,6 +35,13 @@ public class Player : MonoBehaviour
     {
         _rigid.velocity = new Vector2(_inputDirection.x * _moveSpeed, _rigid.velocity.y);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            _bJump = 0;
+        }
+    }
 
     public void _OnMove(InputAction.CallbackContext context)
     {
@@ -37,8 +49,9 @@ public class Player : MonoBehaviour
     }
     public void _OnJump(InputAction.CallbackContext context)
     {
-        if(!context.performed) return;
+        if(!context.performed || _bJump > _jumpMaxCount ) return;
 
         _rigid.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+        _bJump++;
     }
 }
